a9c9b5dafeceebdcb074d8829f5365759c8fe845
garshakurthi saikumar <saikumarg781@gmail.com>
2022-11-01 21:52:53 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+
+const App = () => {
+  const [input, setinput] = useState("");
+  const handler = (e) => {
+    setinput(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calcu">
+      <p>Calculator</p>
+      <input
+        className="init"
+        type="text"
+        value={input}
+        name="input"
+        onChange={handler}
+      />
+      <button className="erace" onClick={() => setinput("")}>
+        c
+      </button>
+      <br />
+      <button className="one" onClick={() => setinput(input + "1")}>
+        1
+      </button>
+      <button className="two" onClick={() => setinput(input + "2")}>
+        2
+      </button>
+      <button className="three" onClick={() => setinput(input + "3")}>
+        3
+      </button>
+      <button className="divi" onClick={() => setinput(input + "/")}>
+        /
+      </button>
+      <br />
+      <button className="four" onClick={() => setinput(input + "4")}>
+        4
+      </button>
+      <button className="five" onClick={() => setinput(input + "5")}>
+        5
+      </button>
+      <button className="six" onClick={() => setinput(input + "6")}>
+        6
+      </button>
+      <button className="substract" onClick={() => setinput(input + "-")}>
+        -
+      </button>
+      <br />
+      <button className="seven" onClick={() => setinput(input + "7")}>
+        7
+      </button>
+      <button className="eight" onClick={() => setinput(input + "8")}>
+        8
+      </button>
+      <button className="nine" onClick={() => setinput(input + "9")}>
+        9
+      </button>
+      <button className="plus" onClick={() => setinput(input + "+")}>
+        +
+      </button>
+      <br />
+      <button className="dot" onClick={() => setinput(input + ".")}>
+        .
+      </button>
+      <button className="zero" onClick={() => setinput(input + "0")}>
+        0
+      </button>
+      <button
+        className="equal"
+        onClick={() => {
+          setinput(eval(input));
+        }}
+      >
+        =
+      </button>
+      <button className="multipli" onClick={() => setinput(input + "*")}>
+        *
+      </button>
     </div>
   );
-}
-
+};
 export default App;
+
+
+
+
+
+
+
+
+
+
